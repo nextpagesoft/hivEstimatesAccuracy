1aba787413eccc9601c5a92f9e98d6cd6615ff57
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-04-11 12:14:29 +0200

    Return table and artifacts from "PreProcessInputDataForMice"

--- R/PreProcessInputDataForMice.R
+++ R/PreProcessInputDataForMice.R
@@ -26,12 +26,9 @@ PreProcessInputDataForMice <- function(inputData)
   inputDataGender <- setDT(mice::complete(miceImputation, action = 1))
   inputData[selGenderMissing2, Gender := inputDataGender$Gender[selGenderMissing2]]
 
-  numGenderMissing1 <- sum(selGenderMissing1)
-  numGenderMissing2 <- sum(selGenderMissing2)
-  numRecords <- nrow(inputData)
-  cat(sprintf("%d (and %f%% of total records) of missing gender cases were replaced with \"Male\" because transmission was reported as \"MSM\".\n", numGenderMissing1, numGenderMissing1 / numRecords),
-      sprintf("%d (and %f%% of total records) cases with missing gender were imputed through single imputation as this is required for the main multiple imputations procedures.\n", numGenderMissing2, numGenderMissing2 / numRecords),
-      sep = "")
+  results <- list(Table = inputData,
+                  Artifacts = list(MissGenderReplaced = sum(selGenderMissing1),
+                                   MissGenderImputed = sum(selGenderMissing2)))
 
-  return(inputData)
+  return(results)
 }
