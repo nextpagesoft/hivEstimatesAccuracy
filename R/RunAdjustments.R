55a445d5e927db751baea56e8aea612a702f4b03
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-04-11 12:15:45 +0200

    Run log text adjusted

--- R/RunAdjustments.R
+++ R/RunAdjustments.R
@@ -29,22 +29,24 @@ RunAdjustments <- function(data, adjustmentSpecs = list())
   for (adjustmentSpec in adjustmentSpecs) {
     i <- i + 1
 
-    caption <- paste(i, adjustmentSpec$Name, sep = ".")
+    caption <- sprintf("%d. %s", i, adjustmentSpec$Name)
     if (!"Key" %in% names(adjustmentSpec)) {
       adjustmentSpec$Key <- caption
     }
 
     cat("--------------------------------------------------------------------------------\n")
-    cat("### Executing adjustment: ", caption, "\n\n", sep = "")
+    cat("## Executing adjustment: ", caption, "\n\n", sep = "")
 
     # Extract parameters for better visibility.
     parameters <- GetParamInfoFromAdjustSpec(adjustmentSpec$Parameters,
                                              infoType = "value")
 
-    cat("## Parameters:\n")
-    print(parameters)
+    cat("# Parameters:\n")
+    print(setNames(as.character(parameters),
+                   names(parameters)))
+    cat("\n")
 
-    cat("## Adjustment text output:\n")
+    cat("# Adjustment text output:\n")
 
     # Run adjustment function
     output <- adjustmentSpec$AdjustmentFunction(inputData = data$Table,
