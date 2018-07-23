7dd3453167ef383b8fe0914c4ec66d2acd313fd8
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-07-23 10:14:25 +0200

    Update dependencies

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(hivEstimatesAccuracy)
-
-test_check("hivEstimatesAccuracy")
--- tests/testthat/test_initial.R
+++
@@ -1,5 +0,0 @@
-context("Initial test")
-
-test_that("2 + 2 is 4", {
-  expect_equal(2 + 2, 4)
-})
