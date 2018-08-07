58100e2510038962d596bac778e96e88d8bdb539
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-08-07 13:25:27 +0200

    Expand countryData with TESSyCode dimension

--- R/PreProcessInputData.R
+++ R/PreProcessInputData.R
@@ -25,10 +25,6 @@ PreProcessInputData <- function(inputData)
   charColNames <- names(colClasses[colClasses == "character"])
   inputData[, (charColNames) := lapply(.SD, toupper), .SDcols = charColNames]
 
-  # Cache country codes for countries of Sub-Saharan Africa
-  ssaCountryCodes <- countryData[SubRegionName == "Sub-Saharan Africa",
-                                 Code]
-
   # Replace UNKs and BLANKS with NAs
   for (i in charColNames) {
     inputData[get(i) %chin% c("UNK", ""), (i) := NA]
@@ -37,121 +33,68 @@ PreProcessInputData <- function(inputData)
   # Drop unused levels (prior UNKs)
   inputData <- droplevels(inputData)
 
-  AUSTNZ_countries <-
-    c('AU','CC','CX','HM','NF','NZ')
+  inputData <- merge(inputData,
+                     countryData[, .(CountryOfBirth = Code, RegionOfBirth = TESSyCode)],
+                     by = c("CountryOfBirth"),
+                     all.x = TRUE)
+  inputData[!is.na(CountryOfBirth) & CountryOfBirth %chin% ReportingCountry,
+            RegionOfBirth := "REPCOUNTRY"]
 
-  CAR_countries <-
-    c('AG','AI','AW','BB','BL','BM','BQ','BS','BV','CU','CW','DM','DO', 'GD','GP','HT','JM','KN',
-      'KY','LC','MF','MQ','MS','PR','SX','TC', 'TT', 'VC','VG','VI')
-
-  CENTEUR_countries <-
-    c('AL','BA','BG','CY','CZ','HR','HU','ME','MK','PL','RO','RS','SI', 'SK','TR')
-
-  EASTASIAPAC_countries <-
-    c('AS','CK','CN','FJ','HK','JP','KP','KR','MN','MO','NU','PF', 'PG','SB','TK','TO','TV','TW',
-      'VU','WF','WS')
-
-  EASTEUR_countries <-
-    c('AM','AZ','BY','EE','GE','KG','KZ','LT','LV','MD','RU','TJ','TM', 'UA','UZ')
-
-  LATAM_countries <-
-    c('AR','BO','BR','BZ','CL','CO','CR','EC','FK','GF','GS','GT','GY', 'HN','MX','NI','PA','PE',
-      'PY','SR','SV','UY','VE')
-
-  NORTHAFRMIDEAST_countries <-
-    c('AE','BH','DZ','EG','EH','IQ','JO','KW','LB','LY','MA', 'OM','PS','QA','SA','SD','SS','SY',
-      'TN','YE')
-
-  NORTHAM_countries <-
-    c('CA','PM','US')
-
-  SOUTHASIA_countries <-
-    c('AF','BD','BN','BT','FM','GU','ID','IN','IR','KH','KI','LA','LK', 'MH','MM','MP','MV','MY',
-      'NC','NP','NR','PH','PK','PN','PW','SG', 'TH','TL','UM','VN')
-
-  SUBAFR_countries <-
-    c('AO','BF','BI','BJ','BW','CD','CF','CG','CI','CM','CV','DJ','ER', 'ET','GA','GH','GM','GN',
-      'GQ','GW','IO','KE','KM','LR','LS','MG', 'ML','MR','MU','MW','MZ','NA','NE','NG','RE','RW',
-      'SC','SH','SL', 'SN','SO','ST','SZ','TD','TF','TG','TZ','UG','YT','ZA','ZM','ZW')
-
-  UNK_countries <-
-    c('AQ')
-
-  WESTEUR_countries <-
-    c('AD','AT','AX','BE','CH','DE','DK','EL','ES','FI','FO','FR','GG', 'GI','GL','IE','IL','IM',
-      'IS','IT','JE','LI','LU','MC','MT','NL', 'NO','PT','SE','SJ','SM','UK','VA')
-
-  inputData[, RegionOfBirth :=
-              ifelse(!is.na(CountryOfBirth) & CountryOfBirth %chin% ReportingCountry,
-                     "REPCOUNTRY", NA_character_)]
-  inputData[CountryOfBirth %chin% WESTEUR_countries,         RegionOfBirth := "WESTEUR"]
-  inputData[CountryOfBirth %chin% CENTEUR_countries,         RegionOfBirth := "CENTEUR"]
-  inputData[CountryOfBirth %chin% EASTEUR_countries,         RegionOfBirth := "EASTEUR"]
-  inputData[CountryOfBirth %chin% SUBAFR_countries,          RegionOfBirth := "SUBAFR"]
-  inputData[CountryOfBirth %chin% EASTASIAPAC_countries,     RegionOfBirth := "EASTASIAPAC"]
-  inputData[CountryOfBirth %chin% AUSTNZ_countries,          RegionOfBirth := "AUSTNZ"]
-  inputData[CountryOfBirth %chin% SOUTHASIA_countries,       RegionOfBirth := "SOUTHASIA"]
-  inputData[CountryOfBirth %chin% NORTHAFRMIDEAST_countries, RegionOfBirth := "NORTHAFRMIDEAST"]
-  inputData[CountryOfBirth %chin% NORTHAM_countries,         RegionOfBirth := "NORTHAM"]
-  inputData[CountryOfBirth %chin% CAR_countries,             RegionOfBirth := "CAR"]
-  inputData[CountryOfBirth %chin% LATAM_countries,           RegionOfBirth := "LATAM"]
-  inputData[CountryOfBirth %chin% UNK_countries,             RegionOfBirth := "UNK"]
-
-  inputData[, RegionOfNationality :=
-              ifelse(!is.na(CountryOfNationality) & CountryOfNationality %chin% ReportingCountry,
-                     "REPCOUNTRY", NA_character_)]
-  inputData[RegionOfNationality %chin% WESTEUR_countries,         RegionOfNationality := "WESTEUR"]
-  inputData[RegionOfNationality %chin% CENTEUR_countries,         RegionOfNationality := "CENTEUR"]
-  inputData[RegionOfNationality %chin% EASTEUR_countries,         RegionOfNationality := "EASTEUR"]
-  inputData[RegionOfNationality %chin% SUBAFR_countries,          RegionOfNationality := "SUBAFR"]
-  inputData[RegionOfNationality %chin% EASTASIAPAC_countries,     RegionOfNationality := "EASTASIAPAC"]
-  inputData[RegionOfNationality %chin% AUSTNZ_countries,          RegionOfNationality := "AUSTNZ"]
-  inputData[RegionOfNationality %chin% SOUTHASIA_countries,       RegionOfNationality := "SOUTHASIA"]
-  inputData[RegionOfNationality %chin% NORTHAFRMIDEAST_countries, RegionOfNationality := "NORTHAFRMIDEAST"]
-  inputData[RegionOfNationality %chin% NORTHAM_countries,         RegionOfNationality := "NORTHAM"]
-  inputData[RegionOfNationality %chin% CAR_countries,             RegionOfNationality := "CAR"]
-  inputData[RegionOfNationality %chin% LATAM_countries,           RegionOfNationality := "LATAM"]
-  inputData[RegionOfNationality %chin% UNK_countries,             RegionOfNationality := "UNK"]
+  inputData <- merge(inputData,
+                     countryData[, .(CountryOfNationality = Code, RegionOfNationality = TESSyCode)],
+                     by = c("CountryOfNationality"),
+                     all.x = TRUE)
+  inputData[!is.na(CountryOfNationality) & CountryOfNationality %chin% ReportingCountry,
+            RegionOfNationality := "REPCOUNTRY"]
 
   # Create detailed 'Migrant' variable: FullMigr based on Region Of Origin
-  inputData[, FullMigr := RegionOfOrigin]
+  inputData[, FullRegionOfOrigin := RegionOfOrigin]
 
   # Update if missing with Region of Birth
-  inputData[is.na(FullMigr), FullMigr := RegionOfBirth]
+  inputData[is.na(FullRegionOfOrigin), FullRegionOfOrigin := RegionOfBirth]
   # Update if 'ABROAD' with Region of Birth
-  inputData[FullMigr %chin% "ABROAD" & !is.na(RegionOfBirth) & !CountryOfBirth %chin% ReportingCountry,
-            FullMigr := RegionOfBirth]
+  inputData[FullRegionOfOrigin %chin% "ABROAD" &
+              !is.na(RegionOfBirth) &
+              !CountryOfBirth %chin% ReportingCountry,
+            FullRegionOfOrigin := RegionOfBirth]
 
   # Update if missing with Region of Nationality
-  inputData[is.na(FullMigr), FullMigr := RegionOfNationality]
+  inputData[is.na(FullRegionOfOrigin), FullRegionOfOrigin := RegionOfNationality]
   # Update if 'ABROAD' with Region of Nationality
-  inputData[FullMigr %chin% "ABROAD" & !is.na(RegionOfNationality) & !CountryOfNationality %chin% ReportingCountry,
-            FullMigr := RegionOfNationality]
+  inputData[FullRegionOfOrigin %chin% "ABROAD" &
+              !is.na(RegionOfNationality) &
+              !CountryOfNationality %chin% ReportingCountry,
+            FullRegionOfOrigin := RegionOfNationality]
 
-  # Create Migr variable 1, 2, 3...
-  inputData[, Migr := factor(NA, levels = c("Reporting Country", "Other Country", "SSA"))]
+  # Cache country codes for countries of Sub-Saharan Africa
+  ssaCountryCodes <- countryData[SubRegionName == "Sub-Saharan Africa",
+                                 Code]
+
+  # Create GroupOfOrigin variable 1, 2, 3...
+  inputData[, GroupOfOrigin := factor(NA, levels = c("Reporting Country", "Other Country", "SSA"))]
   # ...based on RegionOfOrigin if not NA
-  inputData[is.na(Migr) & !is.na(RegionOfOrigin),
-            Migr := ifelse(RegionOfOrigin == "REPCOUNTRY", 1L,
-                           ifelse(!RegionOfOrigin %in% "SUBAFR", 2L,
-                                  3L))]
+  inputData[is.na(GroupOfOrigin) & !is.na(RegionOfOrigin),
+            GroupOfOrigin := ifelse(RegionOfOrigin == "REPCOUNTRY", 1L,
+                                    ifelse(!RegionOfOrigin %in% "SUBAFR", 2L,
+                                           3L))]
   # ...based on CountryOfBirth if not NA
-  inputData[is.na(Migr) & !is.na(CountryOfBirth),
-            Migr := ifelse(CountryOfBirth == ReportingCountry, 1L,
-                           ifelse(!CountryOfBirth %in% ssaCountryCodes, 2L,
-                                  3L))]
+  inputData[is.na(GroupOfOrigin) & !is.na(CountryOfBirth),
+            GroupOfOrigin := ifelse(CountryOfBirth == ReportingCountry, 1L,
+                                    ifelse(!CountryOfBirth %in% ssaCountryCodes, 2L,
+                                           3L))]
   # ...based on CountryOfNationality if not NA
-  inputData[is.na(Migr) & !is.na(CountryOfNationality),
-            Migr := ifelse(CountryOfNationality == ReportingCountry, 1L,
-                           ifelse(!CountryOfNationality %in% ssaCountryCodes, 2L,
-                                  3L))]
+  inputData[is.na(GroupOfOrigin) & !is.na(CountryOfNationality),
+            GroupOfOrigin := ifelse(CountryOfNationality == ReportingCountry, 1L,
+                                    ifelse(!CountryOfNationality %in% ssaCountryCodes, 2L,
+                                           3L))]
 
   # Transform CD4
   inputData[, SqCD4 := sqrt(FirstCD4Count)]
 
   # AIDS close to diagnosis
   inputData[, AIDS := factor(ifelse(!is.na(DateOfAIDSDiagnosisYear),
-                                    ifelse(DateOfAIDSDiagnosisYear - DateOfDiagnosisYear <= 1L, "AIDS-Yes", "AIDS-No"),
+                                    ifelse(DateOfAIDSDiagnosisYear - DateOfDiagnosisYear <= 1L,
+                                           "AIDS-Yes", "AIDS-No"),
                                     "AIDS-No"))]
 
   # Transform Transmission
--- utils/Download_country_data.R
+++ utils/Download_country_data.R
@@ -2,11 +2,19 @@ conn <- url("https://raw.githubusercontent.com/datasets/country-codes/master/dat
 countryData <- read.csv(conn, stringsAsFactors = FALSE, na.strings = "", encoding = "UTF-8")
 data.table::setDT(countryData)
 
-countryData <- countryData[, .(official_name_en, `ISO3166.1.Alpha.2`, `UNTERM.English.Formal`,
-                               Continent, `Region.Name`, `Sub.region.Name`)]
+countryData <- countryData[, .(
+  official_name_en,
+  `ISO3166.1.Alpha.2`,
+  `UNTERM.English.Formal`,
+  Continent,
+  `Region.Name`,
+  `Sub.region.Name`
+)]
 
-data.table::setnames(countryData,
-                     c("Name", "Code", "FormalName", "ContinentCode", "RegionName", "SubRegionName"))
+data.table::setnames(
+  countryData,
+  c("Name", "Code", "FormalName", "ContinentCode", "RegionName", "SubRegionName")
+)
 
 countryData <- countryData[!is.na(Code)]
 
@@ -25,6 +33,70 @@ countryData[Code == "GB", ":="(
   Code = "UK"
 )]
 
+countryData[, TESSyCode := NA_character_]
+countryData[
+  Code %chin% c('AU', 'CC', 'CX', 'HM', 'NF', 'NZ'),
+  TESSyCode := "AUSTNZ"
+]
+countryData[
+  Code %chin% c('AG', 'AI', 'AW', 'BB', 'BL', 'BM', 'BQ', 'BS', 'BV', 'CU', 'CW', 'DM', 'DO', 'GD',
+                'GP', 'HT', 'JM', 'KN', 'KY', 'LC', 'MF', 'MQ', 'MS', 'PR', 'SX', 'TC', 'TT', 'VC',
+                'VG', 'VI'),
+  TESSyCode := "CAR"
+]
+countryData[
+  Code %chin% c('AL', 'BA', 'BG', 'CY', 'CZ', 'HR', 'HU', 'ME', 'MK', 'PL', 'RO', 'RS', 'SI', 'SK',
+                'TR'),
+  TESSyCode := "CENTEUR"
+]
+countryData[
+  Code %chin% c('AS', 'CK', 'CN', 'FJ', 'HK', 'JP', 'KP', 'KR', 'MN', 'MO', 'NU', 'PF', 'PG', 'SB',
+                'TK', 'TO', 'TV', 'TW', 'VU', 'WF', 'WS'),
+  TESSyCode := "EASTASIAPAC"
+]
+countryData[
+  Code %chin% c('AM', 'AZ', 'BY', 'EE', 'GE', 'KG', 'KZ', 'LT', 'LV', 'MD', 'RU', 'TJ', 'TM', 'UA',
+                'UZ'),
+  TESSyCode := "EASTEUR"
+]
+countryData[
+  Code %chin% c('AR', 'BO', 'BR', 'BZ', 'CL', 'CO', 'CR', 'EC', 'FK', 'GF', 'GS', 'GT', 'GY', 'HN',
+                'MX', 'NI', 'PA', 'PE', 'PY', 'SR', 'SV', 'UY', 'VE'),
+  TESSyCode := "LATAM"
+]
+countryData[
+  Code %chin% c('AE', 'BH', 'DZ', 'EG', 'EH', 'IQ', 'JO', 'KW', 'LB', 'LY', 'MA', 'OM', 'PS', 'QA',
+                'SA', 'SD', 'SS', 'SY', 'TN', 'YE'),
+  TESSyCode := "NORTHAFRMIDEAST"
+]
+countryData[
+  Code %chin% c('CA', 'PM', 'US'),
+  TESSyCode := "NORTHAM"
+]
+countryData[
+  Code %chin% c('AF', 'BD', 'BN', 'BT', 'FM', 'GU', 'ID', 'IN', 'IR', 'KH', 'KI', 'LA', 'LK', 'MH',
+                'MM', 'MP', 'MV', 'MY', 'NC', 'NP', 'NR', 'PH', 'PK', 'PN', 'PW', 'SG', 'TH', 'TL',
+                'UM', 'VN'),
+  TESSyCode := "SOUTHASIA"
+]
+countryData[
+  Code %chin% c('AO', 'BF', 'BI', 'BJ', 'BW', 'CD', 'CF', 'CG', 'CI', 'CM', 'CV', 'DJ', 'ER', 'ET',
+                'GA', 'GH', 'GM', 'GN', 'GQ', 'GW', 'IO', 'KE', 'KM', 'LR', 'LS', 'MG', 'ML', 'MR',
+                'MU', 'MW', 'MZ', 'NA', 'NE', 'NG', 'RE', 'RW', 'SC', 'SH', 'SL', 'SN', 'SO', 'ST',
+                'SZ', 'TD', 'TF', 'TG', 'TZ', 'UG', 'YT', 'ZA', 'ZM', 'ZW'),
+  TESSyCode := "SUBAFR"
+]
+countryData[
+  Code %chin% c('AQ'),
+  TESSyCode := "UNK"
+]
+countryData[
+  Code %chin% c('AD', 'AT', 'AX', 'BE', 'CH', 'DE', 'DK', 'EL', 'ES', 'FI', 'FO', 'FR', 'GG', 'GI',
+                'GL', 'IE', 'IL', 'IM', 'IS', 'IT', 'JE', 'LI', 'LU', 'MC', 'MT', 'NL', 'NO', 'PT',
+                'SE', 'SJ', 'SM', 'UK', 'VA'),
+  TESSyCode := "WESTEUR"
+]
+
 save(countryData,
-     file = "D:/_REPOSITORIES/hivEstimatesAccuracy/data/countryData.rda",
+     file = "D:/_REPOSITORIES/Github/hivEstimatesAccuracy/data/countryData.rda",
      compress = "xz")
