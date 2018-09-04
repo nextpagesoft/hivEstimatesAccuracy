ea5f1dad339fb722ca942183c6940f912c99696f
Daniel Lewandowski <daniel@nextpagesoft.net>
2018-09-04 15:54:56 +0200

    Fix sending events to Google Analytics

--- inst/shiny/www/js/google_analytics.js
+++ inst/shiny/www/js/google_analytics.js
@@ -1,11 +1,22 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-125099925-2');
-$('#sidebarItemExpanded > ul').on('click', 'a', function(elem) {
-  let screenName = $(this).text().trim();
-  gtag('event', 'screen_view', {
-    'app_name': 'HIV Estimates Accuracy',
-    'screen_name': screenName
+gtag('config', 'UA-125099925-2', {
+  'app_name': 'HIV Estimates Accuracy'
+});
+$(function() {
+
+  gtag('event', 'page_loaded', {
+    'event_category': 'screen_change',
+    'event_label': 'Input data upload'
+  });
+
+  $('#sidebarItemExpanded > ul').on('click', 'a', function(elem) {
+    let screenName = $(this).text().trim();
+    gtag('event', 'click', {
+      'event_category': 'screen_change',
+      'event_label': screenName
+    });
   });
 });
+
