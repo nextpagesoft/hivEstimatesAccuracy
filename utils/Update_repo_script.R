7a55eca7f75793887cf1c863a09fbf8b1824b251
Daniel Lewandowski <daniel@nextpagesoft.net>
2019-01-18 22:35:04 +0100

    Update paths in the repository update script

--- utils/Update_repo_script.R
+++ utils/Update_repo_script.R
@@ -4,7 +4,7 @@ devtools::document()
 # Build source and binary versions
 repoPath <- "d:/_DEPLOYMENT/hivEstimatesAccuracy/pkgBuilds"
 dir.create(repoPath, showWarnings = FALSE, recursive = TRUE)
-devtools::build(path = file.path(repoPath, "pkgBuilds"),
+devtools::build(path = repoPath,
                 binary = FALSE)
 devtools::build(path = repoPath,
                 binary = TRUE,
@@ -18,19 +18,19 @@ tarFileName <- paste0("hivEstimatesAccuracy_", version, ".tar.gz")
 zipFileName <- paste0("hivEstimatesAccuracy_", version, ".zip")
 
 # Copy package files to appropriate subfolders
-file.copy(file.path(repoPath, "pkgBuilds", tarFileName),
-          file.path(repoPath, "CRAN", "src", "contrib", tarFileName),
+file.copy(file.path(repoPath, tarFileName),
+          file.path(repoPath, "..", "repo", "src", "contrib", tarFileName),
           overwrite = TRUE)
-file.copy(file.path(repoPath, "pkgBuilds", zipFileName),
-          file.path(repoPath, "CRAN", "bin", "windows", "contrib", "3.5", zipFileName),
+file.copy(file.path(repoPath, zipFileName),
+          file.path(repoPath, "..", "repo", "bin", "windows", "contrib", "3.5", zipFileName),
           overwrite = TRUE)
 
 # Update repo metafiles
-tools::write_PACKAGES(dir = file.path(repoPath, "CRAN", "src", "contrib"), type = "source")
-tools::write_PACKAGES(dir = file.path(repoPath, "CRAN", "bin", "windows", "contrib", "3.5"), type = "win.binary")
+tools::write_PACKAGES(dir = file.path(repoPath, "..", "repo", "src", "contrib"), type = "source")
+tools::write_PACKAGES(dir = file.path(repoPath, "..", "repo", "bin", "windows", "contrib", "3.5"), type = "win.binary")
 
 # Update currect version string
-writeLines(version, file.path(repoPath, "pkgBuilds", "version.txt"))
+writeLines(version, file.path(repoPath, "version.txt"))
 
 # Upload to server
 UploadFiles <- function(fileNames, sourcePath, destPath) {
@@ -43,6 +43,7 @@ UploadFiles <- function(fileNames, sourcePath, destPath) {
 
 ftpPath <- keyring::key_get("HIV-server", "ftpPath")
 userpwd <- keyring::key_get("HIV-server", "userpwd")
+
 
 UploadFiles(fileNames = c("version.txt", tarFileName),
             sourcePath = file.path(repoPath, "pkgBuilds"),
