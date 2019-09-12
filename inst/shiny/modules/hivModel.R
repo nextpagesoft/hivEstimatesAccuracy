bc1ef256dcb629180497f889bf14da5856c32c0b
Daniel Lewandowski <daniel@nextpagesoft.net>
2019-09-12 15:10:59 +0200

    Support bootstrap fits and plotting

--- inst/shiny/modules/hivModel.R
+++ inst/shiny/modules/hivModel.R
@@ -35,21 +35,49 @@ hivModelUI <- function(id)
           solidHeader = FALSE,
           status = 'warning',
           collapsible = TRUE,
-          actionButton(
-            ns('runBtn'),
-            label = 'Run',
-            style = 'background-color: #69b023; color: white'
-          ),
-          shinyjs::disabled(
+          div(
             actionButton(
-              ns('cancelBtn'),
-              label = 'Cancel'
+              ns('runMainBtn'),
+              label = 'Main fit',
+              style = 'background-color: #69b023; color: white'
+            ),
+            shinyjs::disabled(
+              actionButton(
+                ns('cancelMainBtn'),
+                label = 'Cancel'
+              )
+            )
+          ),
+          div(
+            style = 'margin-top: 30px',
+            numericInput(
+              ns('bsNumIter'),
+              label = 'Number of boostrap iterations',
+              value = 20,
+              min = 1,
+              max = 100,
+              step = 1,
+              width = 200
+            ),
+            shinyjs::disabled(
+              actionButton(
+                ns('runBootstrapBtn'),
+                label = 'Bootstrap fits',
+                style = 'background-color: #69b023; color: white'
+              )
+            ),
+            shinyjs::disabled(
+              actionButton(
+                ns('cancelBootstrapBtn'),
+                label = 'Cancel'
+              )
             )
           )
         )
       )
     ),
-    uiOutput(ns('log'))
+    uiOutput(ns('log')),
+    uiOutput(ns('plots'))
   )
 }
 
@@ -68,6 +96,8 @@ hivModel <- function(input, output, session, appStatus)
     Context = NULL,
     Data = NULL,
     MainResults = NULL,
+    BSResultsList = NULL,
+    Plots = NULL,
     Log = NULL
   )
 
@@ -137,14 +167,17 @@ hivModel <- function(input, output, session, appStatus)
     })
   })
 
-  # EVENT: Button "Run" clicked
-  observeEvent(input[['runBtn']], {
-    shinyjs::disable('runBtn')
-    shinyjs::enable('cancelBtn')
+  observeEvent(input[['runMainBtn']], {
+    shinyjs::disable('runMainBtn')
+    shinyjs::enable('cancelMainBtn')
+    shinyjs::disable('runBootstrapBtn')
+    shinyjs::disable('cancelBootstrapBtn')
 
     context <- req(localState$Context)
     data <- req(localState$Data)
     localState$MainResults <- NULL
+    localState$BSResultsList <- NULL
+    localState$Plots <- NULL
     localState$Log <- ''
 
     # Show progress message during task start
@@ -154,11 +187,11 @@ hivModel <- function(input, output, session, appStatus)
     startTime <- Sys.time()
     if (isLinux) {
       task <<- CreateTask({
-        hivModelling::PerformMainFit(context, data)
+        hivModelling::PerformMainFit(context, data, maxNoFit = 20, ctol = 1e-5, ftol = 1e-4)
       })
     } else {
       task <<- CreateTask(function(context, data) {
-        hivModelling::PerformMainFit(context, data)
+        hivModelling::PerformMainFit(context, data, maxNoFit = 20, ctol = 1e-5, ftol = 1e-4)
       },
       args = list(context, data))
     }
@@ -178,6 +211,7 @@ hivModel <- function(input, output, session, appStatus)
         localState$Log <- 'Main fit cancelled'
       }
       localState$Log <- paste(
+        paste('* Main fit'),
         paste('Start time  :', FormatTime(startTime)),
         paste('End time    :', FormatTime(endTime)),
         paste('Elapsed time:', FormatDiffTime(endTime - startTime)),
@@ -191,17 +225,114 @@ hivModel <- function(input, output, session, appStatus)
 
       # Close the progress indicator and update button state
       prog$close()
-      shinyjs::enable('runBtn')
-      shinyjs::disable('cancelBtn')
+      shinyjs::enable('runMainBtn')
+      shinyjs::disable('cancelMainBtn')
     })
   })
 
-  # EVENT: Button "Cancel" clicked
-  observeEvent(input[['cancelBtn']], {
+  observeEvent(input[['cancelMainBtn']], {
     req(task)$cancel()
   })
 
-  # Populate adjustment parameter widgets in the editing dialog
+  observeEvent(localState$MainResults, {
+    shinyjs::enable('runBootstrapBtn')
+    shinyjs::disable('cancelBootstrapBtn')
+    mainResults <- localState$MainResults
+    bsResultsList <- localState$BSResultsList
+    plots <- hivModelling::CreateOutputPlots(mainResults, bsResultsList)
+    localState$Plots <- plots
+  })
+
+  observeEvent(input[['runBootstrapBtn']], {
+    shinyjs::disable('runMainBtn')
+    shinyjs::disable('cancelMainBtn')
+    shinyjs::disable('runBootstrapBtn')
+    shinyjs::enable('cancelBootstrapBtn')
+
+    context <- req(localState$Context)
+    data <- req(localState$Data)
+    mainResults <- req(localState$MainResults)
+    bsCount <- req(input$bsNumIter)
+
+    localState$BSResultsList <- NULL
+
+    # Show progress message during task start
+    prog <- Progress$new(session)
+    prog$set(message = 'Performing boostrap fits...', value = 0.1)
+
+    startTime <- Sys.time()
+    if (isLinux) {
+      task <<- CreateTask({
+        hivModelling::PerformBootstrapFits(
+          bsCount = 20,
+          context,
+          data,
+          mainResults,
+          algorithm = 'NLOPT_LN_BOBYQA',
+          executionPlan = future::sequential
+        )
+      })
+    } else {
+      task <<- CreateTask(function(context, data) {
+        hivModelling::PerformBootstrapFits(
+          bsCount = 20,
+          context,
+          data,
+          mainResults,
+          algorithm = 'NLOPT_LN_BOBYQA',
+          executionPlan = future::multiprocess
+        )
+      },
+      args = list(bsCount, context, data, mainResults))
+    }
+
+    o <- observe({
+      # Only proceed when the task is completed (this could mean success,
+      # failure, or cancellation)
+      req(task$completed())
+      endTime <- Sys.time()
+
+      bsResultsList <- task$result()
+      task <<- NULL
+      if (is.list(mainResults)) {
+        localState$BSResultsList <- bsResultsList
+        msg <- ''
+      } else {
+        localState$BSResultsList <- NULL
+        msg <- 'Bootstrap fits cancelled'
+      }
+      localState$Log <- paste(
+        localState$Log,
+        paste(''),
+        paste('* Bootstrap fits'),
+        paste('Start time  :', FormatTime(startTime)),
+        paste('End time    :', FormatTime(endTime)),
+        paste('Elapsed time:', FormatDiffTime(endTime - startTime)),
+        msg,
+        sep = '\n'
+      )
+
+      # This observer runs only once
+      o$destroy()
+
+      # Close the progress indicator and update button state
+      prog$close()
+      shinyjs::enable('runBootstrapBtn')
+      shinyjs::disable('cancelBootstrapBtn')
+    })
+  })
+
+  observeEvent(input[['cancelBootstrapBtn']], {
+    req(task)$cancel()
+  })
+
+  observeEvent(localState$BSResultsList, {
+    mainResults <- localState$MainResults
+    bsResultsList <- localState$BSResultsList
+    plots <- hivModelling::CreateOutputPlots(mainResults, bsResultsList)
+    localState$Plots <- plots
+  })
+
   output[['log']] <- renderUI({
     runLogHTML <- box(
       width = 12,
@@ -212,6 +343,73 @@ hivModel <- function(input, output, session, appStatus)
       tags$pre(req(localState$Log))
     )
     return(runLogHTML)
+  })
+
+  output[['plots']] <- renderUI({
+    req(localState$Plots)
+    plotsHTML <- box(
+      width = 12,
+      title = 'Plots',
+      solidHeader = FALSE,
+      collapsible = TRUE,
+      status = 'warning',
+      plotOutput(ns('N_HIV_Obs_M')),
+      plotOutput(ns('N_CD4_1_Obs_M')),
+      plotOutput(ns('N_CD4_2_Obs_M')),
+      plotOutput(ns('N_CD4_3_Obs_M')),
+      plotOutput(ns('N_CD4_4_Obs_M')),
+      plotOutput(ns('N_HIVAIDS_Obs_M')),
+      plotOutput(ns('N_AIDS_M')),
+      plotOutput(ns('N_Inf_M')),
+      plotOutput(ns('t_diag')),
+      plotOutput(ns('N_Alive')),
+      plotOutput(ns('N_Und_Alive_p'))
+    )
+    return(plotsHTML)
+  })
+
+  output[['N_HIV_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV diagnoses, total']]
+  })
+
+  output[['N_CD4_1_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV diagnoses, CD4 >= 500']]
+  })
+
+  output[['N_CD4_2_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV diagnoses, CD4 >= 350-499']]
+  })
+
+  output[['N_CD4_3_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV diagnoses, CD4 >= 200-349']]
+  })
+
+  output[['N_CD4_4_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV diagnoses, CD4 < 200']]
+  })
+
+  output[['N_HIVAIDS_Obs_M']] <- renderPlot({
+    localState$Plots[['HIV/AIDS diagnoses']]
+  })
+
+  output[['N_AIDS_M']] <- renderPlot({
+    localState$Plots[['AIDS diagnoses, total']]
+  })
+
+  output[['N_Inf_M']] <- renderPlot({
+    localState$Plots[['HIV infections per year']]
+  })
+
+  output[['t_diag']] <- renderPlot({
+    localState$Plots[['Time to diagnosis']]
+  })
+
+  output[['N_Alive']] <- renderPlot({
+    localState$Plots[['Total number of HIV-infected']]
+  })
+
+  output[['N_Und_Alive_p']] <- renderPlot({
+    localState$Plots[['Proportion undiagnosed of all those alive']]
   })
 
   return(NULL)
