67eed0271f2bb43d2feb4549797c846452f9a743
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-01-21 17:25:00 +0100

    Place intro text on the welcome page

--- inst/shiny/modules/welcome.R
+++ inst/shiny/modules/welcome.R
@@ -7,8 +7,25 @@ welcomeUI <- function(id)
   ns <- NS(id)
 
   tagList(
+    div(style = "margin-top: 15px"),
+    div(
+      id = ns("introductionBox"),
+      box(
+        width = 12,
+        fluidRow(
+          column(
+            offset = 3,
+            width = 6,
+            style = "text-align: center; font-weight: bold",
+            p("The ECDC HIV Estimates Accuracy Tool is an application that uses advanced statistical methods to correct for missing values in key HIV surveillance variables as well as for reporting delay, as defined by the time from case diagnosis to notification at the national level."),
+            p("The tool accepts case based HIV surveillance data prepared in a specific format."),
+            p("The outputs include results from pre-defined analyses in the form of a report containing tables and graphs, and datasets, in which the adjustments have been incorporated and which may be exported for further analysis.",
+              style = "margin-bottom: 0")
+          )
+        )
+      )
+    ),
     fluidRow(
-      style = "margin-top: 20px",
       column(
         width = 3, offset = 1, style = "text-align: center",
         actionLink(ns("accuracy"), label = "HIV Accuracy", icon("calculator"))
