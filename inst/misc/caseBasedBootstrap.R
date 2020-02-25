d46a6f27926e762d715c21ffdd3c3a61c0a5b889
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-02-25 21:58:57 +0100

    Update dependencies

--- inst/misc/caseBasedBootstrap.R
+++ inst/misc/caseBasedBootstrap.R
@@ -26,24 +26,8 @@ bootstrapCount <- 20
 # data, but this work is not finalized yet.
 parameters <- list(
   INCIDENCE = list(
-    ModelMinYear = 1980,
-    ModelMaxYear = 2017,
-    FitPosMinYear = 1979,
-    FitPosMaxYear = 1979,
-    FitPosCD4MinYear = 1985,
-    FitPosCD4MaxYear = 2015,
-    FitAIDSMinYear = 1985,
-    FitAIDSMaxYear = 1995,
-    FitAIDSPosMinYear = 1985,
-    FitAIDSPosMaxYear = 2015,
     ModelNoKnots = 4,
-    FitDistribution = 'POISSON',
-    Intervals = data.table(
-      StartYear = c(1980L, 1984L, 1992L, 2000L, 2008L),
-      Jump = c(FALSE, TRUE, TRUE, TRUE, TRUE),
-      ChangeInInterval = c(FALSE, FALSE, FALSE, FALSE, FALSE),
-      DiffByCD4 = c(FALSE, FALSE, FALSE, FALSE, FALSE)
-    )
+    FitDistribution = 'POISSON'
   )
 )
 
